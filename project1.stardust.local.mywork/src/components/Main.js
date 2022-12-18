fd26bf43f32e6a6cdb95ff0e7abe04963333061e
sehyoungchoi <hope69034@gmail.com>
2022-12-19 01:23:21 +0900

    build main

--- project1.stardust.local.mywork/src/components/Main.js
+++ project1.stardust.local.mywork/src/components/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
 <div style={{marginLeft:'20px', marginTop:'10px'}}>
  
  
- <h1 className="ffff">   제 첫번째 프로젝트에서 제가 구현한 것을 올리는 페이지입니다.</h1>
+ <h1 className="ffff">   제 첫번째 프로젝트에서 제가 구현한 것만 올리는 페이지입니다.</h1>
  <br></br> 
  &nbsp;&nbsp;&nbsp;프로젝트 주제 : 두피 자가진단 서비스 <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 기간 : 2022.08.16. ~ 2022.09.01 <br></br>
