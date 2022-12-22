bdbb32d7ee74bd6601109a2b966e4761f2b4f148
sehyoungchoi <hope69034@gmail.com>
2022-12-22 14:26:07 +0900

    test navbar

--- main.react/src/components/Navbar.js
+++ main.react/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return(
   <>
-    <Link to='/' style={{textDecoration: 'none'}}> &nbsp;&nbsp;Main </Link>
+    <Link to='/' style={{textDecoration: 'none'}}> &nbsp;&nbsp;&nbsp;Main </Link>
     <Link to='/Project' style={{textDecoration: 'none'}}> Project </Link>
     <Link to='/GJAIacademy' style={{textDecoration: 'none'}}> GJAIacademy </Link>
     <Link to='/GJAIsupporters' style={{textDecoration: 'none'}}> GJAIsupporters </Link>
