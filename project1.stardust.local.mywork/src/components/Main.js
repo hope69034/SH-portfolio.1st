70027ac03ac6b643a3e168a53e64aa7688e527c8
hope69034 <hope69034@gmail.com>
2022-12-23 01:12:04 +0900

    d

--- project1.stardust.local.mywork/src/components/Main.js
+++ project1.stardust.local.mywork/src/components/Main.js
@@ -9,9 +9,24 @@ const Main = () => {
  
  <h1 className="ffff">   제 첫번째 프로젝트에서 제가 구현한 것만 올리는 페이지입니다.</h1>
  <br></br> 
- &nbsp;&nbsp;&nbsp;프로젝트 주제 : 두피 자가진단 서비스 <br></br>
+ &nbsp;&nbsp;&nbsp;프로젝트 제목 : 두피 자가진단 서비스 <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 기간 : 2022.08.16 ~ 2022.09.01 <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 인원 : 4명 (광주인공지능사관학교 3기)<br></br>
+ &nbsp;&nbsp;&nbsp;프로젝트 내용 : aihub상 데이터와 train코드를 바탕으로
+ 짧은 기간 진행된 첫 프로젝트이다. <br></br> 
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;
+ 두피 이미지를 업로드 받으면 각 두피질환의 중증도 및 두피타입을 알려주고 그에 맞는 솔루션을 제시한다.<br></br> 
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;
+ 여건상 배치사이즈를 크게 주지 못해 60~70%의 낮은 아큐러시로 구현되었다.
+ 
+ 
+
+ <br></br>
+ &nbsp;&nbsp;&nbsp;기술 스택 : python, pytorch, efficientNet, flask, html, css, ngrok<br></br>
  &nbsp;&nbsp;&nbsp;<a style={{textDecoration: 'none'}} target="_blank" href='https://velog.io/@shlog/%EC%B2%AB%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EA%B8%B0%EB%A1%9D'>개발일지 : velog</a><br></br>
 &nbsp;&nbsp;&nbsp;<a style={{textDecoration: 'none'}} target="_blank" href='https://github.com/hope69034/GJAI-Academy-Main/tree/main/%EA%B4%91%EC%A3%BC%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5%EC%82%AC%EA%B4%80%ED%95%99%EA%B5%90%202022.08/JS_B_Stardust_Project'>코드 : github</a><br></br>
 
--- project2.greenee.front.mywork/src/page/DoorPage.js
+++ project2.greenee.front.mywork/src/page/DoorPage.js
@@ -8,11 +8,21 @@ const DoorPage = () => {
   <div>
   <br></br>  <br></br>
  
- <h1 className="ffff">   제 두번째 프로젝트에서 제가 구현한 것 중 일부를 올리는 페이지입니다.</h1>
+ <h1 className="ffff">   &nbsp;&nbsp;&nbsp; 제 두번째 프로젝트에서 제가 구현한 것 중 일부를 <br></br>  올리는 페이지입니다.</h1>
  <br></br>
- &nbsp;&nbsp;&nbsp;프로젝트 주제 : 일정관리 x 챗봇 앱 - AI 비서 '그리니' <br></br>
+ &nbsp;&nbsp;&nbsp;프로젝트 제목 : 일정관리 x 챗봇 앱 - AI 비서 '그리니' <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 기간 : 2022.11.08 ~ 2022.12.14 <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 인원 : 6명 (광주인공지능사관학교 3기)<br></br>
+ &nbsp;&nbsp;&nbsp;프로젝트 내용 : 일정앱인 그리니는 ai를 활용한 자동 알림/추천을 메인 기능으로 기획되었다. 이 기능을 챗봇에 연동하여 대화형식(버튼, 링크, 대화)<br></br> 
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;
+ 으로 알림/추천을 하고 챗봇과의 대화데이터를 DB에 저장한 후 ai로 분석하여 더 나은 자동 알림/추천을 하겠다는 프로젝트이다.  <br></br>
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+ &nbsp;&nbsp;
+ 하지만 기획과는 다르게 알림이 구현되지 못해 발전방향만 장황할 뿐 챗봇의 존재가 무색해진 프로젝트이다.<br></br>
+ &nbsp;&nbsp;&nbsp;기술 스택 : react, node.js, html, css, GPT3, bootstrap, netlify<br></br>
  &nbsp;&nbsp;&nbsp;<a target="_blank" href='https://velog.io/@shlog/project.nd'>개발일지 : velog</a><br></br>
 &nbsp;&nbsp;&nbsp;<a target="_blank" href='https://github.com/hope69034/portfolio.1st/tree/main/project2.greenee.front.mywork'>코드 : github</a><br></br>
 &nbsp;&nbsp;&nbsp;<a target="_blank" href='https://greenee.netlify.app/'>앱 프론트엔드 배포 : netlify </a><br></br>
