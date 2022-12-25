bc138f43c03976ad0bd8da5d0b89fa9098597621
hope69034 <hope69034@gmail.com>
2022-12-25 23:40:32 +0900

    p1 title cent

--- project1.stardust.local.mywork/src/components/Main.js
+++ project1.stardust.local.mywork/src/components/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import bc1 from '../img/bc1.png'
 const Main = () => {
-
+  const sss = {textAlign:'center', fontSize:'25px',fontWeight:'900'}
 
 const bc1style ={    width: '400px',
   margin: 'auto',
@@ -13,7 +13,7 @@ const bc1style ={    width: '400px',
 <div style={{marginLeft:'20px', marginTop:'10px'}}>
  
  
- <h2 className="ffff">   제 첫번째 프로젝트에서 제가 구현한 것만 올리는 페이지입니다.</h2>
+ <h2  style={sss}   >   제 첫번째 프로젝트에서 제가 구현한 것만 올리는 페이지입니다.</h2>
 
  &nbsp;&nbsp;&nbsp;프로젝트 제목 : 두피 자가진단 서비스 <br></br>
  &nbsp;&nbsp;&nbsp;프로젝트 기간 : 2022.08.16 ~ 2022.09.01 <br></br>
